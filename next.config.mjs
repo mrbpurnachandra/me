9d4c56cd22fdd8c95b648360d19de4db1a5131a4
Prakash Bhattarai <lightvelocity999@gmail.com>
2024-02-28 19:13:48 +0545

    Change

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  distDir: "docs"
+  assetPrefix: "/",
+  trailingSlash: true,
+  distDir: "docs",
 };
 
 export default nextConfig;
